a7680af0542877a86dbf76c0b476fc89a59d4462
VanNguyen <vannguyen10995@gmail.com>
2017-10-27 11:48:04 +0700

    first commit

---
+++ app/src/main/java/info/androidhive/parsenotifications/app/AppConfig.java
@@ -0,0 +1,11 @@
+package info.androidhive.parsenotifications.app;
+
+/**
+ * Created by Ravi on 15/05/15.
+ */
+public class AppConfig {
+    public static final String PARSE_CHANNEL = "AndroidHive";
+    public static final String PARSE_APPLICATION_ID = "wQFLn2nDPDCsh9lo0emiMwLYP2CgERnebD5cr71F";
+    public static final String PARSE_CLIENT_KEY = "bps1sMX0dTur01iHfJb6DGfFQ9HPaixKgHGKfEKt";
+    public static final int NOTIFICATION_ID = 100;
+}
